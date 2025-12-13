1463e3cea30532ec4528f4c714dd994c0967f21b
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-12-13 23:19:08 +0500

    Create input-password-group-field.tsx

---
+++ shadcn/forms/input-password-group-field.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import Link from "next/link";
+
+import { Field, FieldDescription, FieldError, FieldLabel } from "@/components/ui/field";
+import { Controller, Control, FieldError as RHFError, FieldPath, FieldValues } from "react-hook-form";
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/components/ui/input-group";
+
+import { cn } from "@/lib/utils";
+import { Eye, EyeOff } from "lucide-react";
+import { FORGOT_PASSWORD_URL } from "@/constants/route-links";
+
+type InputPasswordGroupFieldProps<T extends FieldValues> = {
+  control: Control<T>;
+  name: FieldPath<T>;
+  label?: string;
+  description?: string;
+  inputProps?: React.ComponentProps<"input">;
+  isShowForgotPasswordLink?: boolean;
+};
+
+const InputPasswordGroupField = <T extends FieldValues>({
+  control,
+  name,
+  label = "Description",
+  description = "",
+  inputProps = {
+    placeholder: "Enter your password",
+  },
+  isShowForgotPasswordLink = false,
+}: InputPasswordGroupFieldProps<T>) => {
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+
+  const togglePasswordVisibility = () => setIsPasswordVisible((prevState) => !prevState);
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <div className={cn(isShowForgotPasswordLink ? "flex items-center" : "")}>
+            {label && <FieldLabel htmlFor={name}>{label}</FieldLabel>}
+            {isShowForgotPasswordLink && (
+              <Link
+                href={FORGOT_PASSWORD_URL}
+                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </Link>
+            )}
+          </div>
+          <InputGroup className="relative">
+            <InputGroupInput {...field} {...inputProps} type={isPasswordVisible ? "text" : "password"} />
+            <InputGroupAddon align="inline-end">
+              <InputGroupButton onClick={togglePasswordVisibility}>
+                {isPasswordVisible ? <Eye size={18} /> : <EyeOff size={18} />}
+              </InputGroupButton>
+            </InputGroupAddon>
+          </InputGroup>
+          {description && <FieldDescription>{description}</FieldDescription>}
+          {fieldState.invalid && <FieldError errors={[fieldState.error as RHFError]} />}
+        </Field>
+      )}
+    />
+  );
+};
+
+export default InputPasswordGroupField;
+
+/**
+ * Usage
+ * 
+  <InputPasswordGroupField
+    control={form.control}
+    name="password"
+    label="Password"
+    inputProps={{
+      type: "password",
+      placeholder: "Enter your password",
+    }}
+    isShowForgotPasswordLink={true}
+  />
+ */
