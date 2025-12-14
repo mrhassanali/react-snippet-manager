ae925a08e5846bebfe917f130a6c403b47c97ac6
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-12-14 14:14:40 +0500

    Update use-current-user.tsx

--- shadcn/hooks/use-current-user.tsx
+++ shadcn/hooks/use-current-user.tsx
@@ -1,35 +1,20 @@
-import { createClient } from "@/lib/supabase/client";
 import { useEffect, useState } from "react";
 
-type UserProfile = {
-  full_name: string;
-  email: string;
-  avatar: string;
-  age: number;
-  address: string;
-  phone_number: string;
-};
+import { createClient } from "@/lib/supabase/client";
+import { type User } from "@supabase/supabase-js";
 
 export const useCurrentUser = () => {
-  const [user, setUser] = useState<UserProfile | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const fetchProfileName = async () => {
-        
       const { data, error } = await createClient().auth.getSession();
 
       if (error) {
         console.error(error);
       }
 
-      setUser({
-        full_name: data.session?.user.user_metadata.full_name ?? "Hassan Ali",
-        email: data.session?.user.email ?? "?",
-        avatar: data.session?.user.user_metadata.avatar ?? "",
-        age: data.session?.user.user_metadata.age ?? 0,
-        address: data.session?.user.user_metadata.address ?? "",
-        phone_number: data.session?.user.user_metadata.phone_number ?? "",
-      });
+      setUser(data.session?.user || null);
     };
 
     fetchProfileName();
